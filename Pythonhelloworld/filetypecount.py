10d85d832f886f8d16f76ecd6a157b411dc51821
RichardSchrauwen <RichardSchrauwen@users.noreply.github.com>
2020-01-03 10:23:59 +0100

    added null checks

--- Pythonhelloworld/filetypecount.py
+++ Pythonhelloworld/filetypecount.py
@@ -15,24 +15,22 @@ config.read('example.ini')
 myPath = config['filetypecount']['path']
 searchExtension = config['filetypecount']['searchExtension']
 searchString = config['filetypecount']['searchString']
+#print(f"searchString = '{searchString}'")
 
 # check if username needs to be replaced with the OS username
 if "$USER" in myPath:
     myPath = myPath.replace("$USER", user)
     print(f"Path to use = {myPath}")
 
-# count all files
-#file_count = sum((len(f) for _, _, f in os.walk(myPath)))
-#print("Total file count = {}".format(file_count))
-
-# count a specific file_extension type
+# count a specific file extension type and how many times the searchString is present
 string_count = 0
 extension_count = 0
 other_count = 0
 
+# recursively walk the way
 for root, dirs, files in os.walk(myPath):
     for file in files:
-        if searchString in file:
+        if searchString and searchString is not None and searchString in file:
             string_count += 1
         filename, file_extension = os.path.splitext(file)
         if file_extension.lower() == searchExtension:
@@ -40,6 +38,8 @@ for root, dirs, files in os.walk(myPath):
         else:
             other_count += 1
 
+# print results
 file_count = extension_count + other_count
-print("file count : {0} + {1} = {2}".format(extension_count, other_count, file_count))
+print("file count : {0} + {1} (other) = {2}".format(extension_count, other_count, file_count))
+print("file count with extension '{0}' = {1}".format(searchExtension, extension_count))
 print("file count containing '{0}' = {1}".format(searchString, string_count))

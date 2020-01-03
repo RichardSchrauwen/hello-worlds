3fd7f129741dc4efe913c742d903a82a86a54814
RichardSchrauwen <RichardSchrauwen@users.noreply.github.com>
2020-01-03 10:57:33 +0100

    enhanced printing config params

--- Pythonhelloworld/parseconfigfile.py
+++ Pythonhelloworld/parseconfigfile.py
@@ -1,6 +1,11 @@
 import configparser
 config = configparser.ConfigParser()
 config.read('example.ini')
-print(f"Config section found: {config.sections()}")
+print(f"Config section(s) found: {config.sections()}")
 print(config['DEFAULT']['path'])
 print(config['mp3count']['path'])
+
+for row in config.sections():
+    print(row)
+    for key in config[row]:
+        print(f"   {key} = {config[row][key]}")

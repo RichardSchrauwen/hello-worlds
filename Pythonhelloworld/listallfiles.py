e1f5815c6e15b9d4d36f7cbdfa3e34287eb6ae93
RichardSchrauwen <RichardSchrauwen@users.noreply.github.com>
2020-01-06 15:03:34 +0100

    moved functionality to new file

---
+++ Pythonhelloworld/listallfiles.py
@@ -0,0 +1,25 @@
+import os
+from os import listdir
+from os.path import isfile, join
+import time
+from argparse import ArgumentParser
+
+# command line parameters
+parser = ArgumentParser()
+parser.add_argument("-d", "--dir", dest="directory",
+                    help="The directory to read", metavar="DIR")
+parser.add_argument("-q", "--quiet",
+                    action="store_false", dest="verbose", default=True,
+                    help="don't print status messages to stdout")
+args = parser.parse_args()
+
+# get the path from command line (default is current directory)
+mypath = "."
+if args.directory != None:
+    mypath = args.directory
+
+start = time.time()
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+print('files are {}'.format(onlyfiles))
+elapsed_time = int(time.time() - start)
+print("elapsed_time: {}".format(elapsed_time))
---
+++ Pythonhelloworld/numberoffiles.py
@@ -0,0 +1,60 @@
+import os
+from os import listdir
+from os.path import isfile, join
+import datetime
+from argparse import ArgumentParser
+
+# command line parameters
+parser = ArgumentParser()
+parser.add_argument("-d", "--dir", dest="directory",
+                    help="The directory to read", metavar="DIR")
+parser.add_argument("-t", "--top", dest="top", type=int,
+                    help="the top number of file types", metavar="TOP")
+parser.add_argument("-q", "--quiet",
+                    action="store_false", dest="verbose", default=True,
+                    help="don't print status messages to stdout")
+args = parser.parse_args()
+
+# get the path from command line (default is current directory)
+mypath = "."
+if args.directory != None:
+    mypath = args.directory
+
+# get the top n from command line (default is top 5)
+top = 5
+if args.top != None:
+    top = args.top
+
+start = datetime.datetime.now()
+d = {}
+
+# recursively walk the way
+for root, dirs, files in os.walk(mypath):
+    for file in files:
+        _ , file_extension = os.path.splitext(file)
+        if file_extension in d.keys():
+            d[file_extension] += 1
+        else:
+            d[file_extension] = 1
+
+# print the results
+#print(f"totals per extension: {d.items()}")
+sorted_d = sorted(d.items(), key=lambda kv: kv[1], reverse=True)
+print(f"sorted totals per extension: {sorted_d}")
+
+print(f"path = {mypath} checking for top {top} file extensions")
+i = 0
+for f in sorted_d:
+    if i < top:
+        i += 1
+        print(f"#{i}    {f}  ")
+
+# some calculations
+print(f"total number of files: {sum(d.values())}")
+
+
+end = datetime.datetime.now()
+delta = end - start
+#print(delta)
+elapsed_time = int(delta.total_seconds() * 1000)
+print("elapsed_time: {} milliseconds.".format(elapsed_time))

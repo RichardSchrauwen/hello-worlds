c9eb3a2b555eafe319cdeea8c81012235626986d
RichardSchrauwen <RichardSchrauwen@users.noreply.github.com>
2020-01-25 21:15:44 +0100

    Add AWS SQS files

---
+++ AWShelloworld/aws-listSQSqueues.py
@@ -0,0 +1,9 @@
+import boto3
+
+# Get the service resource
+sqs = boto3.resource('sqs')
+
+# Print out each queue name, which is part of its ARN
+for queue in sqs.queues.all():
+    print(queue.url)
+    print(queue.attributes.get('DelaySeconds'))

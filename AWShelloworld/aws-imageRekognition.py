4c6ec55d8867077c277de2494825cf0d2a5f2818
RichardSchrauwen <RichardSchrauwen@users.noreply.github.com>
2020-01-27 13:07:01 +0100

    Added cli parameters

--- AWShelloworld/aws-imageRekognition.py
+++ AWShelloworld/aws-imageRekognition.py
@@ -2,7 +2,32 @@
 #PDX-License-Identifier: MIT-0 (For details, see https://github.com/awsdocs/amazon-rekognition-developer-guide/blob/master/LICENSE-SAMPLECODE.)
 
 import boto3
+from argparse import ArgumentParser
 
+# command line parameters
+parser = ArgumentParser()
+parser.add_argument("-b", "--bucket", dest="bucket",
+                    help="The S3 bucket to use", metavar="BUCK")
+parser.add_argument("-n", "--name", dest="name",
+                    help="file name to feed into Rekognition", metavar="NAME")
+parser.add_argument("-q", "--quiet",
+                    action="store_false", dest="verbose", default=True,
+                    help="don't print status messages to stdout")
+args = parser.parse_args()
+
+# get the bucket from command line (default is pictures-to-analyze)
+bucket = "pictures-to-analyze"
+if args.bucket != None:
+    bucket = args.bucket
+
+# get the file name from command line
+if args.name == None:
+    print("Oops, no file name was entered. Please use '-n <name>' arg.")
+    sys.exit()
+name = args.name
+print(f"Analyzing '{name}' from S3 {bucket}")
+
+# rekognition function call
 def detect_labels(photo, bucket):
 
     client=boto3.client('rekognition')
@@ -34,9 +59,7 @@ def detect_labels(photo, bucket):
 
 
 def main():
-    photo='test1.jpg'
-    bucket='pictures-to-analyze'
-    label_count=detect_labels(photo, bucket)
+    label_count=detect_labels(name, bucket)
     print("Labels detected: " + str(label_count))
 
 
--- AWShelloworld/aws-uploadS3file.py
+++ AWShelloworld/aws-uploadS3file.py
@@ -1,8 +1,32 @@
 import logging
 import boto3
 from botocore.exceptions import ClientError
+from argparse import ArgumentParser
 
+# command line parameters
+parser = ArgumentParser()
+parser.add_argument("-b", "--bucket", dest="bucket",
+                    help="The S3 bucket to use", metavar="BUCK")
+parser.add_argument("-n", "--name", dest="name",
+                    help="file name to upload", metavar="NAME")
+parser.add_argument("-q", "--quiet",
+                    action="store_false", dest="verbose", default=True,
+                    help="don't print status messages to stdout")
+args = parser.parse_args()
 
+# get the bucket from command line (default is pictures-to-analyze)
+bucket = "pictures-to-analyze"
+if args.bucket != None:
+    bucket = args.bucket
+
+# get the file name from command line
+if args.name == None:
+    print("Oops, no file name was entered. Please use '-n <name>' arg.")
+    sys.exit()
+name = args.name
+print(f"Uploading '{name}' to S3 {bucket}")
+
+# S3 function call
 def upload_file(file_name, bucket, object_name=None):
     """Upload a file to an S3 bucket
 
@@ -25,8 +49,10 @@ def upload_file(file_name, bucket, object_name=None):
         return False
     return True
 
-file_name = "test1.jpg"
-bucket = "pictures-to-analyze"
-print(f"Upload file {file_name} to bucket {bucket}")
-if (upload_file(file_name, bucket)):
+#file_name = "test1.jpg"
+#bucket = "pictures-to-analyze"
+print(f"Upload file {name} to bucket {bucket}")
+if (upload_file(name, bucket)):
     print("success")
+else:
+    print("failed")

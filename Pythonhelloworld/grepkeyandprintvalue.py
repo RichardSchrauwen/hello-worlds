2dce7d79fce87ef7fe6061263ac706f38460c38b
RichardSchrauwen <RichardSchrauwen@users.noreply.github.com>
2021-07-21 13:11:18 +0200

    rename

--- Pythonhelloworld/grepkeyandprintvalue.py
+++ Pythonhelloworld/grepkeyandprintvalue.py
@@ -36,7 +36,7 @@ if args.limit != None and args.limit > 0:
     limit = args.limit
 
 # output file
-myfiles = "lines.txt"
+myfiles = "1.txt"
 if args.output != None:
     myfiles = args.output
 
@@ -46,7 +46,7 @@ if args.grep != None:
     searchpattern = args.grep
 mypattern = re.compile(searchpattern)
 print(f"In '{mypath}' looking for '{searchpattern}'")
-print(f"Checking for file type '{myext}' above limit {limit} bytes in {mypath}")
+print(f"Checking file types '{myext}' with more than {limit} bytes in {mypath}")
 
 # function to get file size in bytes
 def file_bytesize(file_path):
@@ -60,45 +60,38 @@ def file_bytesize(file_path):
             b = 0
         return b
 
-# list all files and send output to a file if conditions match
+# parse all files and send output to a file if conditions match
 start = time.time()
 nr_files = 0
 with open(myfiles, "w", encoding="utf-8") as filewrite:
     # recursively walk the way through my path
     for root, dirs, files in os.walk(mypath):
         for file in files:
+            if limit > 0:
+                file_path = os.path.join(root, file)
+                size = file_bytesize(file_path)
+                if size != None and size <= limit:
+                    # do nothing
+                    print(f"File {file} is below {limit}, going to next file")
+                    continue
+
             if myext != "*":
                 _ , file_extension = os.path.splitext(file)
-                if file_extension == myext:
-                    # grep
-                    with open(file) as origin_file:
-                        for line in origin_file:
-                            match = mypattern.search(line)
-                            if match:
-                                result = mypattern.split(line)
-                                #print(result[1])
-                                filewrite.write(f"{result[1]}\n")
+                if file_extension != myext:
+                    # do nothing
+                    print(f"File {file} has extension {file_extension}, going to next file")
+                    continue
 
-            else:
-                if limit > 0:
-                    file_path = os.path.join(root, file)
-                    size = file_bytesize(file_path)
-                    if size != None and size > limit:
-                        filewrite.write(f"{file_path},{size}\n")
+            with open(file) as origin_file:
+                for line in origin_file:
+                    match = mypattern.search(line)
+                    if match:
+                        result = mypattern.split(line)
+                        #print(result[1])
+                        filewrite.write(f"{result[1]}")
                         nr_files += 1
-                else:
-                    #filewrite.write(f"{os.path.join(root, file)}\n")
-                    # grep
-                    with open(file) as origin_file:
-                        for line in origin_file:
-                            match = mypattern.search(line)
-                            if match:
-                                result = mypattern.split(line)
-                                #print(result[1])
-                                filewrite.write(f"result[1]\n")
-                    nr_files += 1
 
-print(f"Written {nr_files} file names in {myfiles}")
+print(f"Found {nr_files} files with {searchpattern}. Output to {myfiles}")
 
 elapsed_time = int(time.time() - start)
 print("elapsed_time: {}".format(elapsed_time))
